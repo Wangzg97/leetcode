5fb163d625d7915851f5548a623eaac86dd38471
Wangzg97 <1948811388@qq.com>
2020-01-18 21:31:16 +0800

    整数反转

---
+++ src/ByTitleNumber/reverse.java
@@ -0,0 +1,18 @@
+package ByTitleNumber;
+
+public class reverse {
+    // 判断溢出：大于整数最大值MAX_VALUE或小于整数最小值MIN_VALUE
+    public int solution1(int x) {
+        int result = 0;
+        while (x != 0) {
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && x % 10 > 7))
+                return 0;
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && x % 10 < -8))
+                return 0;
+            result = result * 10 + x % 10;
+            x = x / 10;
+        }
+        return result;
+    }
+
+}
